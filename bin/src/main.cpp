28e1deee9751e17c39e299b69336a19babbb66f9
Ian A.B. King <iansens@hotmail.com>
2019-06-14 01:42:57 +0200

    visual data map extraction tool command

--- bin/src/main.cpp
+++ bin/src/main.cpp
@@ -15,6 +15,7 @@ using namespace xsteg;
 // xsteg -d -t SATURATION UP 0.5 1210 -ii tt2.png -o
 // xsteg -d -t SATURATION UP 0.5 1210 -ii tt2.png -of hello2.txt
 // xsteg -m -t SATURATION UP 0.5 1210 -ii tt2.png -oi tt3.png
+// xsteg -vd -ii tt2.png
 
 std::map<std::string, visual_data_type> visual_data_type_name_map = 
 {
@@ -39,7 +40,8 @@ enum class encode_mode
     NOT_SET,
     ENCODE,
     DECODE,
-    DIFF_MAP
+    DIFF_MAP,
+    VDATA_MAPS
 };
 
 void str_toupper(std::string& str)
@@ -157,6 +159,10 @@ main_args parse_main_args(int argc, char** argv)
         {
             result.mode = encode_mode::DIFF_MAP;
         }
+        else if(arg == "-vd")
+        {
+            result.mode = encode_mode::VDATA_MAPS;
+        }
     }
     return result;
 }
@@ -202,24 +208,74 @@ void diff_map(main_args args)
     diff_map.write_to_file(args.output_img);
 }
 
+void vdata_maps(main_args args)
+{
+    image img(args.input_img);
+
+    auto log_gen = [](const std::string& type) -> void
+    {
+        std::cout << "Generating visual data map [" << type << "]" << std::endl;
+    };
+
+    log_gen("ALPHA");
+    image vmap_alpha = generate_visual_data_map(&img, visual_data_type::ALPHA);
+    log_gen("AVERAGE_VALUE_RGB");
+    image vmap_avgrgb = generate_visual_data_map(&img, visual_data_type::AVERAGE_VALUE_RGB);
+    log_gen("AVERAGE_VALUE_RGBA");
+    image vmap_avgrgba = generate_visual_data_map(&img, visual_data_type::AVERAGE_VALUE_RGBA);
+    log_gen("COLOR_BLUE");
+    image vmap_b = generate_visual_data_map(&img, visual_data_type::COLOR_BLUE);
+    log_gen("COLOR_GREEN");
+    image vmap_g = generate_visual_data_map(&img, visual_data_type::COLOR_GREEN);
+    log_gen("COLOR_RED");
+    image vmap_r = generate_visual_data_map(&img, visual_data_type::COLOR_RED);
+    log_gen("LUMINANCE");
+    image vmap_lum = generate_visual_data_map(&img, visual_data_type::LUMINANCE);
+    log_gen("SATURATION");
+    image vmap_sat = generate_visual_data_map(&img, visual_data_type::SATURATION);
+
+    std::cout << "Saving files..." << std::endl;
+    vmap_alpha.write_to_file(args.input_img + ".ALPHA.png");
+    vmap_avgrgb.write_to_file(args.input_img + ".AVERAGE_VALUE_RGB.png");
+    vmap_avgrgba.write_to_file(args.input_img + ".AVERAGE_VALUE_RGBA.png");
+    vmap_b.write_to_file(args.input_img + ".COLOR_BLUE.png");
+    vmap_g.write_to_file(args.input_img + ".COLOR_GREEN.png");
+    vmap_r.write_to_file(args.input_img + ".COLOR_RED.png");
+    vmap_lum.write_to_file(args.input_img + ".LUMINANCE.png");
+    vmap_sat.write_to_file(args.input_img + ".SATURATION.png");
+    std::cout << "Done!" << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     main_args margs = parse_main_args(argc, argv);
-    if(margs.mode == encode_mode::NOT_SET)
+    switch (margs.mode)
     {
-        std::cerr << "Encoding mode not set!" << std::endl;
-    }
-    else if(margs.mode == encode_mode::ENCODE)
-    {
-        encode(margs);
-    }
-    else if(margs.mode == encode_mode::DECODE)
-    {
-        decode(margs);
-    }
-    else if(margs.mode == encode_mode::DIFF_MAP)
-    {
-        diff_map(margs);
+        case encode_mode::NOT_SET:
+        {
+            std::cerr << "Encoding mode not set!" << std::endl;
+            return -1;
+        }
+        case encode_mode::ENCODE:
+        {
+            encode(margs);
+            break;
+        }
+        case encode_mode::DECODE:
+        {
+            decode(margs);
+            break;
+        }
+        case encode_mode::DIFF_MAP:
+        {
+            diff_map(margs);
+            break;
+        }
+        case encode_mode::VDATA_MAPS:
+        {
+            vdata_maps(margs);
+            break;
+        }
     }
     return 0;
 }
